6597dacdcfe589d04e8d49418500c97f3e2b644f
HamiDuong <hd028@hdm-stuttgart.de>
2022-04-13 15:21:27 +0200

    BO für Break, ProjectDuration, ProjectWork, TimeIntervall, Vacation fertig

--- src/server/bo/ProjectDurationBO.py
+++ src/server/bo/ProjectDurationBO.py
@@ -2,10 +2,10 @@ from server.bo import TimeIntervallBO as ti
 
 """
 @author Ha Mi Duong (https://github.com/HamiDuong)
-VacationBO ist eine Subklasse von TimeIntervallBO und stellt den Urlaub eines Mitarbeiters da
+ProjectDurationBO ist eine Subklasse von TimeIntervallBO und stellt die Laufzeit eines Projekts da
 Weil TimeIntervallBO bereits von BusinessObject erbt, muss diese Klasse nicht nochmal importiert werden
 """
-class VacationBO (ti.TimeIntervallBO):
+class ProjectDurationBO (ti.TimeIntervallBO):
     """
     Konstruktor der Klasse TimeIntervallBO
     geerbte Attribute
@@ -14,19 +14,30 @@ class VacationBO (ti.TimeIntervallBO):
         _end: Enpunkt des Zeitintervalls (TimeIntervallBO)
         _time_intervall_booking_id: Fremdschlüssel zum Objekt TimeIntervallBookingBO für die eindeutige
                                     Zuordnung zwischen Intervallbuchung und Zeitintervall (TimeIntervallBO)
+    Attribute
+        _project_id: Fremdschlüssel zum Object ProjectBO für die eindeutige Zuordnung zwischen Projektdauer und Projekt
     """
     def __init__(self):
         super().__init__()
+        self._project_id = 0
+
+    'Getter und Setter Methoden'
+    def get_project_id(self):
+        return self._project_id
+
+    def set_project_id(self, id):
+        self._project_id = id
 
     'Gibt die Werte eines Objekts der Klasse in Textform zurück'
     def __str__(self):
-         return "Urlaub {}: von {} bis {}, gehört zur Intervall-Buchung mit der ID {}".format(self.get_id(), self.get_start(), self.get_end(), self.get_time_intervall_booking_id())
+        return "ProjectDuration {}, von {} bist {}, von Projekt {}".format(self.get_id(), self.get_start(), self.get_end(), self.get_project_id())
 
-    'wandelt ein Python dict() in ein VacationBO'
+    'wandelt ein Python dict() in ein ProjectDurationBO'
     def from_dict(dictionary=dict()):
-        obj = VacationBO()
+        obj = ProjectDurationBO()
         obj.set_id(dictionary["id"])
         obj.set_start(dictionary["start"])
         obj.set_end(dictionary["end"])
         obj.set_time_intervall_booking_id(dictionary["time_intervall_booking_id"])
+        obj.set_project_id(dictionary["project_id"])
         return obj
