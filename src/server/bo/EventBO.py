2de88fe3baefe1c0e859be622f0113b2679276b2
khadidja-kebaili <80825550+khadidja-kebaili@users.noreply.github.com>
2022-04-20 11:53:28 +0200

    Module und imports geändert

--- src/server/bo/EventBO.py
+++ src/server/bo/EventBO.py
@@ -1,9 +1,11 @@
-from bo import BusinessObject
+from server.bo import BusinessObject as bo
 
-class EventBO(BusinessObject):
+
+class EventBO(bo.BusinessObject):
     """Klasse Event.
-    Ein Event ist ein Ereignis mit einem Zeitpunkt???
+    Ein Event ist ein Ereignis mit einem Zeitpunkt
     """
+
     def __init__(self):
         super().__init__()
         self._time = None
@@ -17,26 +19,26 @@ class EventBO(BusinessObject):
         """Setzen der ID."""
         self._time = datetime
 
-    def get_booking_id(self):
+    def get_event_booking_id(self):
         """Auslesen der Ereignisbuchung-ID."""
         return self._event_booking_id
 
-    def set_booking_id(self, booking_id):
+    def set_event_booking_id(self, booking_id):
         """Setzen der Ereignisbuchung-ID."""
         self._event_booking_id = booking_id
 
     def __str__(self):
         """Erzeugen einer einfachen textuellen Darstellung der jeweiligen Instanz.
-        
+
         Diese besteht aus der ID der Superklasse ergänzt durch den Zeitpunkt des
         des jeweiligen Events."""
-        return "Customer: {}, {}, {}, {}".format(self.get_id(), self.get_date_of_last_change(), self._time, self._event_booking_id)
+        return "EventBO {}, {}, {}, {}".format(self.get_id(), self.get_date_of_last_change(), self.get_time(), self.get_event_booking_id())
 
     @staticmethod
     def from_dict(dictionary=dict()):
-        """Umwandeln eines Python dict() in einen Customer()."""
+        """Umwandeln eines Python dict() in ein EventBO()."""
         obj = EventBO()
         obj.set_id(dictionary["id"])  # eigentlich Teil von BusinessObject !
-        obj.set_first_name(dictionary["time"])
-        obj.set_last_name(dictionary["eventbooking_id"])
+        obj.set_time(dictionary["time"])
+        obj.set_event_booking_id(dictionary["eventbooking_id"])
         return obj
--- src/server/bo/GoingBO.py
+++ src/server/bo/GoingBO.py
@@ -1,16 +1,19 @@
 from multiprocessing import Event
-from bo import EventBO
+from server.bo import EventBO
 
-class GoingBO(EventBO):
-    """Klasse Going.
-    Ein Going ist ein Ereignis mit einem Zeitpunkt???
+
+class GoingBO(EventBO.EventBO):
     """
+    Klasse Going.
+    Ein ComingBO stellt das Ereignis "Kommen" dar bzw. wenn ein Mitarbeiter sich ausstempelt und enthält einen Zeitpunkt.
+    """
+
     def __init__(self):
         super().__init__()
 
     @staticmethod
     def from_dict(dictionary=dict()):
-        """Umwandeln eines Python dict() in einen Customer()."""
+        """Umwandeln eines Python dict() in ein GoingBO()."""
         obj = GoingBO()
         obj.set_id(dictionary["id"])  # eigentlich Teil von BusinessObject !
         obj.set_first_name(dictionary["time"])
