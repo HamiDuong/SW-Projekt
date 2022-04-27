e39de2736742c8194bd3d17d24a83293868cbcc5
khadidja-kebaili <80825550+khadidja-kebaili@users.noreply.github.com>
2022-04-27 10:54:36 +0200

    Funktionen für VacationBegin -/ bzw. End in Businesslogik eingefügt

--- src/server/Businesslogic.py
+++ src/server/Businesslogic.py
@@ -1,8 +1,11 @@
 from bo.ComingBO import ComingBO
 from db.ComingMapper import ComingMapper
 from bo.GoingBO import GoingBO
-from db.ComingMapper import ComingMapper
 from db.GoingMapper import GoingMapper
+from bo.VacationBeginBO import VacationBeginBO
+from db.VacationBeginMapper import VacationBeginMapper
+from bo.VacationEndBO import VacationEndBO
+from db.VacationEndMapper import VacationEndMapper
 from datetime import datetime
 
 
@@ -57,10 +60,48 @@ class Businesslogic (object):
         with GoingMapper() as mapper:
             mapper.delete(going)
 
+    def create_vacation_begin(self, time, event_booking_id):
+        vacation_begin = VacationBeginBO()
+        vacation_begin.set_time(time)
+        vacation_begin.set_event_booking_id(event_booking_id)
+        with VacationBeginMapper() as mapper:
+            return mapper.insert(vacation_begin)
 
-'''
-b = Businesslogic()
-today = datetime.today()
-timestamp = datetime.timestamp
-b.create_going(today, 1)
-'''
+    def get_vacation_begin_by_id(self, number):
+        with VacationBeginMapper() as mapper:
+            return mapper.find_by_key(number)
+
+    def get_all_vacation_begins(self):
+        with VacationBeginMapper() as mapper:
+            return mapper.find_all()
+
+    def save_vacation_begin(self, vacation_begin):
+        with VacationBeginMapper() as mapper:
+            mapper.update(vacation_begin)
+
+    def delete_vacation_begin(self, vacation_begin):
+        with VacationBeginMapper() as mapper:
+            mapper.delete(vacation_begin)
+
+    def create_vacation_end(self, time, event_booking_id):
+        vacation_end = VacationEndBO()
+        vacation_end.set_time(time)
+        vacation_end.set_event_booking_id(event_booking_id)
+        with VacationEndMapper() as mapper:
+            return mapper.insert(vacation_end)
+
+    def get_vacation_end_by_id(self, number):
+        with VacationEndMapper() as mapper:
+            return mapper.find_by_key(number)
+
+    def get_all_vacation_ends(self):
+        with VacationEndMapper() as mapper:
+            return mapper.find_all()
+
+    def save_vacation_end(self, vacation_end):
+        with VacationEndMapper() as mapper:
+            mapper.update(vacation_end)
+
+    def delete_vacation_end(self, vacation_end):
+        with VacationEndMapper() as mapper:
+            mapper.delete(vacation_end)
