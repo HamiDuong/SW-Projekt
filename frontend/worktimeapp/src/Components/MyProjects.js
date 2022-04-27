b093e4896e27144bc26bce41ccfb8811a2898f8a
vinamle <vl036@hdm-stuttgart.de>
2022-04-27 12:47:06 +0200

    Erstellung MyProject und CreateProject

--- frontend/worktimeapp/src/Components/MyProjects.js
+++ frontend/worktimeapp/src/Components/MyProjects.js
@@ -1,1 +1,14 @@
+import React, { Component } from 'react';
 
+
+class MyProjects extends Component {
+    constructor(props) {
+        super(props);
+    }
+    state = {  }
+    render() { 
+        return ( <h1>Hallo</h1> );
+    }
+}
+ 
+export default MyProjects;
