97811fa94a4f9f50269599fb64660cddc8408c07
HamiDuong <hd028@hdm-stuttgart.de>
2022-04-30 15:41:35 +0200

    Grundstruktur für MyBookings

--- frontend/worktimeapp/src/Components/MyBookingsEntry.js
+++ frontend/worktimeapp/src/Components/MyBookingsEntry.js
@@ -1,4 +1,10 @@
+import { TableContainer } from '@mui/material';
 import React, {Component} from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
 
 class MyBookingsEntry extends Component {
     constructor(props) {
@@ -8,10 +14,28 @@ class MyBookingsEntry extends Component {
             error: null,
         }
     }
-    state = {  }
+
+    //Erstellung eines Eintrags in der Tabelle
+    createEntry(type, name, start, ende){
+        return {type, name, start, ende};
+    }
+
     render() { 
         return (
-            <div></div>
+            <div>
+                <p>Hier startet ein Eintrag</p>
+                    <Table aria-label="simple table">
+                        <TableHead>
+                        <TableRow>
+                            <TableCell align="right">Name</TableCell>
+                            <TableCell align="right">Start</TableCell>
+                            <TableCell align="right">Ende</TableCell>
+                        </TableRow>
+                        </TableHead>
+                    </Table>
+            </div>
+
+
         );
     }
 }
