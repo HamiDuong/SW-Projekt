0610a64a28932363142d8022554a5bb38cdccac2
Mihriban Dogan <md121@hdm-stuttgart.de>
2022-05-08 23:29:40 +0200

    Erstellung EventBookingMapper

---
+++ src/server/db/EventBookingMapper.py
@@ -0,0 +1,129 @@
+from server.bo.EventBookingBO import EventBookingBO
+from server.db.Mapper import Mapper
+
+
+class EventBookingMapper (Mapper):
+
+    def __init__(self):
+        super().__init__()
+
+    def find_all(self):
+        """Auslesen aller Event Bookings.
+        """
+        result = []
+        cursor = self._cnx.cursor()
+        cursor.execute(
+            "SELECT id, dateOfLastChange, bookingId, eventId from eventbookings")
+        tuples = cursor.fetchall()
+
+        for (id, dateOfLastChange, bookingId, eventId) in tuples:
+            eventbooking = EventBookingBO()
+            eventbooking.set_id(id)
+            eventbooking.set_booking_id(dateOfLastChange)
+            eventbooking.set_event_id(eventId)
+            eventbooking.set_date_of_last_change(bookingId)
+
+            result.append(eventbooking)
+
+        self._cnx.commit()
+        cursor.close()
+
+        return result
+
+    def find_by_booking_id(self, bookingId):
+        """Auslesen aller Event Bookings eines bestimmten Zeitkontos.
+        """
+        result = []
+        cursor = self._cnx.cursor()
+        command = "SELECT id, dateOfLastChange, bookingId, eventId from eventbookings WHERE bookingId={} ORDER BY id".format(
+            bookingId)
+        cursor.execute(command)
+        tuples = cursor.fetchall()
+
+        for (id, dateOfLastChange, bookingId, eventId) in tuples:
+            eventbooking = EventBookingBO()
+            eventbooking.set_id(id)
+            eventbooking.set_booking_id(dateOfLastChange)
+            eventbooking.set_event_id(eventId)
+            eventbooking.set_date_of_last_change(bookingId)
+
+            result.append(eventbooking)
+
+        self._cnx.commit()
+        cursor.close()
+
+        return result
+
+    def find_by_event_id(self, eventId):
+        """ Auslesen aller Bookings nach eventsIds. 
+        """
+        result = []
+        cursor = self._cnx.cursor()
+        command = "SELECT id, dateOfLastChange, bookingId, eventId from eventbookings WHERE eventId={} ORDER BY id".format(
+            eventId)
+        cursor.execute(command)
+        tuples = cursor.fetchall()
+
+        for (id, dateOfLastChange, bookingId, eventId) in tuples:
+            eventbooking = EventBookingBO()
+            eventbooking.set_id(id)
+            eventbooking.set_booking_id(dateOfLastChange)
+            eventbooking.set_event_id(eventId)
+            eventbooking.set_date_of_last_change(bookingId)
+
+            result.append(eventbooking)
+
+        self._cnx.commit()
+        cursor.close()
+
+        return result
+
+    def insert(self, eventbooking):
+        """Einfügen eines EventBooking-Objekts in die Datenbank.
+        """
+        cursor = self._cnx.cursor()
+        cursor.execute("SELECT MAX(id) AS maxid FROM eventbookings")
+        tuples = cursor.fetchall()
+
+        for (maxid) in tuples:
+            eventbooking.set_id(maxid[0]+1)
+
+        command = "INSERT INTO eventbookings (id, dateOfLastChange, bookingId, eventId) VALUES (%s,%s,%s,%s)"
+        data = (eventbooking.get_id(), eventbooking.get_date_of_last_change(
+        ), eventbooking.get_booking_id(), eventbooking.get_event_id())
+        cursor.execute(command, data)
+
+        self._cnx.commit()
+        cursor.close()
+        return eventbooking
+
+    def update(self, eventbooking):
+        """Wiederholtes Schreiben eines Objekts in die Datenbank.
+        """
+        cursor = self._cnx.cursor()
+
+        command = "UPDATE eventbookings " + "SET dateOfLastChange=%s WHERE id=%s"
+        data = (eventbooking.get_date_of_last_change(), eventbooking.get_id())
+        cursor.execute(command, data)
+
+        self._cnx.commit()
+        cursor.close()
+
+    def delete(self, eventbooking):
+        """Löschen der Daten eines Booking-Objekts aus der Datenbank.
+        """
+        cursor = self._cnx.cursor()
+
+        command = "DELETE FROM eventbookings WHERE id={}".format(
+            eventbooking.get_id())
+        cursor.execute(command)
+
+        self._cnx.commit()
+        cursor.close()
+
+
+if (__name__ == "__main__"):
+    with EventBookingMapper() as mapper:
+        result = mapper.find_all()
+        for p in result:
+            print(p)
