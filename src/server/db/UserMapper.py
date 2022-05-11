45688e3d44bc1a65bfe87cca5ca65ea49f7689c7
Marcoia <mi022@hdm-stuttgart.de>
2022-05-12 00:47:47 +0200

    UserMapper angepasst

--- src/server/db/UserMapper.py
+++ src/server/db/UserMapper.py
@@ -5,33 +5,33 @@ class UserMapper(Mapper):
     def __init__(self):
         super().__init__()
 
-    def insert(self, event):
+    def insert(self, user):
         cursor = self._cnx.cursor()
         cursor.execute("SELECT MAX(id) AS maxid FROM users ")
         tuples = cursor.fetchall()
 
         for (maxid) in tuples:
             if maxid[0] is not None:
-                event.set_id(maxid[0] + 1)
+                user.set_id(maxid[0] + 1)
             else:
                 """Wenn wir KEINE maximale ID feststellen konnten, dann gehen wir
                 davon aus, dass die Tabelle leer ist und wir mit der ID 1 beginnen können."""
-                event.set_id(1)
+                user.set_id(1)
 
         command = "INSERT INTO users (id, first_name, last_name, mail_adress, user_name) VALUES (%s, %s, %s, %s, %s)"
         data = (
-            event.get_id(),
-            event.get_first_name(),
-            event.get_last_name(),
-            event.get_mail_adress(),
-            event.get_user_name
+            user.get_id(),
+            user.get_first_name(),
+            user.get_last_name(),
+            user.get_mail_adress(),
+            user.get_user_name
             )
 
         cursor.execute(command, data)
 
         self._cnx.commit()
         cursor.close()
-        return event
+        return user
 
     def find_all(self):
 
@@ -42,13 +42,13 @@ class UserMapper(Mapper):
         tuples = cursor.fetchall()
 
         for (id, first_name, last_name, mail_adress, user_name) in tuples:
-            event = UserBO()
-            event.set_id(id)
-            event.set_first_name(first_name)
-            event.set_last_name(last_name)
-            event.set_mail_adress(mail_adress)
-            event.set_user_name(user_name)
-            result.append(event)
+            user = UserBO()
+            user.set_id(id)
+            user.set_first_name(first_name)
+            user.set_last_name(last_name)
+            user.set_mail_adress(mail_adress)
+            user.set_user_name(user_name)
+            result.append(user)
 
         self._cnx.commit()
         cursor.close()
@@ -66,13 +66,13 @@ class UserMapper(Mapper):
 
         try:
             (id, first_name, last_name, mail_adress, user_name) = tuples[0]
-            event = UserBO()
-            event.set_id(id)
-            event.set_first_name(first_name)
-            event.set_last_name(last_name)
-            event.set_mail_adress(mail_adress)
-            event.set_user_name(user_name)
-            result = event
+            user = UserBO()
+            user.set_id(id)
+            user.set_first_name(first_name)
+            user.set_last_name(last_name)
+            user.set_mail_adress(mail_adress)
+            user.set_user_name(user_name)
+            result = user
         except IndexError:
             """Der IndexError wird oben beim Zugriff auf tuples[0] auftreten, wenn der vorherige SELECT-Aufruf
             keine Tupel liefert, sondern tuples = cursor.fetchall() eine leere Sequenz zurück gibt."""
@@ -83,7 +83,7 @@ class UserMapper(Mapper):
 
         return result
 
-    def find_by_name(self, key):
+    '''    def find_by_name(self, key):
         result = []
 
         cursor = self._cnx.cursor()
@@ -93,18 +93,18 @@ class UserMapper(Mapper):
         tuples = cursor.fetchall()
 
         for (id, first_name, last_name, mail_adress, user_name) in tuples:
-            event = UserBO()
-            event.set_id(id)
-            event.set_first_name(first_name)
-            event.set_last_name(last_name)
-            event.set_mail_adress(mail_adress)
-            event.set_user_name(user_name)
-            result.append(event)
+            user = UserBO()
+            user.set_id(id)
+            user.set_first_name(first_name)
+            user.set_last_name(last_name)
+            user.set_mail_adress(mail_adress)
+            user.set_user_name(user_name)
+            result.append(user)
 
         self._cnx.commit()
         cursor.close()
 
-        return result
+        return result'''
 
     def find_by_googleuserid(self, key):
         result = None
@@ -117,13 +117,13 @@ class UserMapper(Mapper):
 
         try:
             (id, first_name, last_name, mail_adress, user_name) = tuples[0]
-            event = UserBO()
-            event.set_id(id)
-            event.set_first_name(first_name)
-            event.set_last_name(last_name)
-            event.set_mail_adress(mail_adress)
-            event.set_user_name(user_name)
-            result = event
+            user = UserBO()
+            user.set_id(id)
+            user.set_first_name(first_name)
+            user.set_last_name(last_name)
+            user.set_mail_adress(mail_adress)
+            user.set_user_name(user_name)
+            result = user
         except IndexError:
             """Der IndexError wird oben beim Zugriff auf tuples[0] auftreten, wenn der vorherige SELECT-Aufruf
             keine Tupel liefert, sondern tuples = cursor.fetchall() eine leere Sequenz zurück gibt."""
@@ -134,48 +134,75 @@ class UserMapper(Mapper):
 
         return result
 
-    def find_by_email(self, key):
+    def find_by_mail_adress(self, key):
         result = []
 
         cursor = self._cnx.cursor()
-        command = "SELECT id, first_name, last_name, mail_adress, user_name FROM users WHERE email={}".format(
+        command = "SELECT id, first_name, last_name, mail_adress, user_name FROM users WHERE mail_adress={}".format(
             key)
         cursor.execute(command)
         tuples = cursor.fetchall()
 
         for (id, first_name, last_name, mail_adress, user_name) in tuples:
-            event = UserBO()
-            event.set_id(id)
-            event.set_first_name(first_name)
-            event.set_last_name(last_name)
-            event.set_mail_adress(mail_adress)
-            event.set_user_name(user_name)
-            result.append(event)
+            user = UserBO()
+            user.set_id(id)
+            user.set_first_name(first_name)
+            user.set_last_name(last_name)
+            user.set_mail_adress(mail_adress)
+            user.set_user_name(user_name)
+            result.append(user)
 
         self._cnx.commit()
         cursor.close()
 
         return result
 
-    def update(self, event):
+
+
+    def find_by_user_name(self, key):
+        result = []
+
+        cursor = self._cnx.cursor()
+        command = "SELECT id, first_name, last_name, mail_adress, user_name FROM users WHERE user_name={}".format(
+            key)
+        cursor.execute(command)
+        tuples = cursor.fetchall()
+
+        for (id, first_name, last_name, mail_adress, user_name) in tuples:
+            user = UserBO()
+            user.set_id(id)
+            user.set_first_name(first_name)
+            user.set_last_name(last_name)
+            user.set_mail_adress(mail_adress)
+            user.set_user_name(user_name)
+            result.append(user)
+
+        self._cnx.commit()
+        cursor.close()
+
+        return result
+
+
+
+    def update(self, user):
         cursor = self._cnx.cursor()
 
         command = "UPDATE users " + \
             "SET first_name=%s, last_name=%s, mail_adress=%s, user_name=%s WHERE id=%s"
-        data = (event.get_first_name(), event.get_last_name(), event.get_mail_adress(), event.get_user_name(),
-                event.get_id())
+        data = (user.get_first_name(), user.get_last_name(), user.get_mail_adress(), user.get_user_name(),
+                user.get_id())
         cursor.execute(command, data)
 
         self._cnx.commit()
         cursor.close()
 
-        return event
+        return user
 
-    def delete(self, event):
+    def delete(self, user):
         cursor = self._cnx.cursor()
 
         command = "DELETE FROM users WHERE id={}".format(
-            event.get_id())
+            user.get_id())
         cursor.execute(command)
 
         self._cnx.commit()
