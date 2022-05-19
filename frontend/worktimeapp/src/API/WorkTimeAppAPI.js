456f1853c1978fe6d79913400ff9e6626c82a4e3
HamiDuong <hd028@hdm-stuttgart.de>
2022-05-19 09:09:32 +0200

    Frontendd API - Test Commit um zu schauen ob sie in GitHub ankommen

--- frontend/worktimeapp/src/API/WorkTimeAppAPI.js
+++ frontend/worktimeapp/src/API/WorkTimeAppAPI.js
@@ -1,4 +1,11 @@
 //Alle BOs importieren
+import TimeIntervalBO from './TimeIntervalBO'
+import BreakBO from './GroupBO';
+import IllnessBO from './IllnessBO';
+import ProjectDurationBO from './ProjectDurationBO';
+import ProjectWorkBO from './ProjectWorkBO';
+import VacationBO from './VacationBO';
+import WorkBO from './WorkBO';
 
 export default class WorkTimeAppAPI{
     static #api = null
@@ -6,6 +13,20 @@ export default class WorkTimeAppAPI{
     #worktimeappServerBaseURL = '/api';
 
     //Hier alle URL Zuweisungen
+
+    //TimeInterval
+
+    //Break
+
+    //Illness
+
+    //ProjectDuration
+
+    //ProjectWork
+
+    //Vacation
+
+    //Work
 
     static getAPI(){
         if(this.#api == null){
