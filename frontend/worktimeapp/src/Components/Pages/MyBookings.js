dc0512b14a23e61a82484da89fcad6c2410056c2
HamiDuong <hd028@hdm-stuttgart.de>
2022-05-28 22:54:23 +0200

    Attribute der Filter werden abgespeichert

--- frontend/worktimeapp/src/Components/Pages/MyBookings.js
+++ frontend/worktimeapp/src/Components/Pages/MyBookings.js
@@ -1,12 +1,7 @@
 import React, {Component} from 'react';
 import { 
-    RadioGroup,
-    Radio, 
-    FormControlLabel, 
     FormControl, 
-    FormLabel, 
     TableHead, 
-    TableSortLabel, 
     TableRow, 
     TableCell, 
     TableContainer,
@@ -14,12 +9,16 @@ import {
     Paper,
     Table,
     TableBody,
-    Button
+    Button,
+    FormControlLabel,
+    Radio,
+    FormLabel,
+    RadioGroup,
+    TextField,
+    InputLabel,
+    MenuItem,
+    Select
 } from '@mui/material';
-import TextField from '@mui/material/TextField';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import Select from '@mui/material/Select';
 
 const header = [
     {
@@ -36,6 +35,13 @@ const header = [
         disablePadding: false,
         label: 'Type'
     },
+/*     {
+        id: 'date',
+        name: 'Date',
+        numeric: false,
+        disablePadding: false,
+        label: 'Date'
+    }, */
     {
         id: 'start',
         name: 'Start Date',
@@ -63,103 +69,300 @@ const bookings = [
     {
         bookingtype:'Event',
         type:'Work',
-        start:'8:00',
-        end:'',
+        date: new Date('1995-12-17T03:24:00').toLocaleString(),
+        start: new Date('2022-05-05T08:30:00').toLocaleString(),
+        end:'-',
         remarks:''
     },
     {
         bookingtype:'Event',
         type:'Work',
-        start:'',
-        end:'18:00',
+        date: new Date('1995-12-17T03:24:00').toLocaleString(),
+        start:'-',
+        end: new Date('2022-05-05T18:30:00').toLocaleString(),
         remarks:''
     },
     {
         bookingtype:'Time Interval',
         type:'Work',
-        start:'8:00',
-        end:'18:00',
+        date: new Date('1995-12-17T03:24:00').toLocaleString(),
+        start:new Date('2022-05-05T08:30:00').toLocaleString(),
+        end:new Date('2022-05-05T18:30:00').toLocaleString(),
         remarks:''
     },
     {
         bookingtype:'Event',
         type:'Break',
+        date: '2022-05-05',
         start:'12:00',
-        end:'',
+        end:'-',
         remarks:''
     },
     {
         bookingtype:'Time Interval',
         type:'Break',
+        date: '2022-05-05',
         start:'12:00',
-        end:'',
+        end:'EMPTY',
         remarks:'Open Interval, no End Event'
-    },
-    {
-        bookingtype:'',
-        type:'',
-        start:'',
-        end:'',
-        remarks:''
-    },
-    {
-        bookingtype:'',
-        type:'',
-        start:'',
-        end:'',
-        remarks:''
-    },
-                    
+    },                    
 ]
+
+let intervalbookings = [
+    {
+        id: '1',
+        dateoflastchange: new Date('2022-05-05T18:30:00').toLocaleString(),
+        start: new Date('2022-05-05T08:30:00').toLocaleString(),
+        end: new Date('2022-05-05T18:30:00').toLocaleString(),
+        startevent: '1',
+        endevent: '2',
+        type: 'Work'
+    },
+    {
+        id: '2',
+        dateoflastchange: new Date('2022-05-05T12:30:00').toLocaleString(),
+        start: new Date('2022-05-05T12:30:00').toLocaleString(),
+        end: null,
+        startevent: '1',
+        endevent: null,
+        type: 'Break'
+    }
+]
+
+// let eventbookings = [
+//     {
+//         id: '1',
+//         dateoflastchange: new Date('2022-05-05T08:30:00'),
+//         time: new Date('2022-05-05T08:30:00')
+//     },
+//     {
+//         id: '2',
+//         dateoflastchange: new Date('2022-05-05T18:30:00'),
+//         time: new Date('2022-05-05T18:30:00')
+//     },
+//     {
+//         id: '1',
+//         dateoflastchange: new Date('2022-05-05T12:30:00'),
+//         time: new Date('2022-05-05T12:30:00')
+//     }
+// ]
+
+
+// let work = WorkBO(new Date('1995-12-17T03:24:00').toLocaleString(), new Date('1995-12-17T03:24:00').toLocaleString(), 1, 2, 'Work');
+// let pause = BreakBO(new Date('1995-12-17T03:24:00').toLocaleString(), new Date('1995-12-17T03:24:00').toLocaleString(), 1, 2, 'Work')
+
+// let hold = [
+//     work,
+//     pause
+// ]
+
+/***
+ * Steps:
+ * alle Eventbuchungen des Users holen -> eventbookingsmit mit EventSubklassenBO
+ * alle Intervalbuchungen des Users holen -> intervalbookings mit IntervalSubklassenBO
+ * 
+ * umwandeln in Date: new Date(this.state.startfilter)
+ */
 
 class MyBookings extends Component {
     constructor(props){
         super(props);
         this.state = {
-            bookings: [],
+            intervalbookings: [],
+            eventbookings: [],
+
+            filterdintervalbookings: [],
+            filterdeventbookings: [],
+
+            bookingtype: 'all',
             typefilter: '',
-            bookingtype: null,
             startfilter: null,
-            endfilter: null, 
+            endfilter: null,
+
             loadingInProgress: false,
+            showResetButton: true,
             error: null,
         }
     }
 
-    handleChange = (e) => {
-        this.setState({ [e.target.name] : e.target.value });}
+    handleChange = ev => {
+        this.setState({ [ev.target.name] : ev.target.value });
+    };
+
+    print = () => {
+        console.log(this.state.bookingtype);
+        console.log(this.state.typefilter);
+        this.setState({
+            showResetButton: false
+        })
+        console.log(this.state.showResetButton);
+        console.log(this.state.startfilter);
+        console.log(this.state.endfilter);
+    }
+
+    componentDidMount(){
+        //Methoden welche direkt aufgerufen werden sollen beim Instanziieren des Objects
+    }
+
+    setFilter = () => {
+        let starthold = document.getElementById("startfilter");
+        let endhold = document.getElementById("endfilter");
+        this.setState({
+            startfilter: starthold.value,
+            endfilter: endhold.value,
+            showResetButton: false
+        }, function(){
+            console.log(this.state.startfilter);
+        });
+    }
+
+    resetFilter = () => {
+        this.setState({
+            bookingtype: 'all',
+            typefilter: '',
+            startfilter: null,
+            endfilter: null,
+            showResetButton: true
+        }, function(){
+            console.log(this.state.startfilter)
+        })
+    }
+
+    printState = () => {
+        console.log(this.state.bookingtype);
+        console.log(this.state.typefilter);
+        console.log(this.state.startfilter);
+        console.log(this.state.endfilter);
+    }
+
+    getIntervalBookings = () => {
+
+    }
+
+    makeRows = () => {
+        let holder = this.state.bookings;
+        let res = []
+        holder.forEach(elem => {
+            let p = {
+                bookingtype: "Interval",
+                type: elem.type,
+                start: elem.start.toLocaleString(),
+                end: elem.end.toLocaleString(),
+                remarks: ""
+            }
+            res.append(p);
+        })
+        this.state({
+            intervalbookings: res
+        })
+        
+    }
+
+    getEventBookings = () => {
+
+    }
+    
+    // filterBookings = () => {
+    //     let start = this.state.startfilter;
+    //     let end = this.state.endfilter;
+
+    //     let holder = this.state.bookings;
+    //     let res = [];
+
+    //     holder.forEach(function(elem){
+    //         console.log(elem);
+    //         if(this.state.startfilter != null){
+
+    //         }
+    //     });
+
+    //     this.setState({
+    //         filteredbookings: res
+    //     })
+    // }
 
     render(){
-        const {} = this.state;
         return(
             <div>
                 <h1>My Bookings</h1>
-                <TextField
-                    id="standard-basic"
-                    label="Start Date"
-                    variant="standard"
-                    padding='10px'
-                />
-                <TextField id="standard-basic" label="End Date" variant="standard" />
-                <FormControl sx={{ minWidth: 258}}>
-                    <InputLabel>Type</InputLabel>
-                    <Select
-                    name='intervaltype'
-                    value={this.state.typefilter}
-                    onChange={this.handleChange}
-                    variant="standard"
+                <div>
+                    <FormControl>
+                        <FormLabel id="viewfilter"></FormLabel>
+                        <RadioGroup
+                            row
+                            name= 'bookingtype'
+                            defaultValue="all"
+                            onChange={this.handleChange}
+                            value={this.state.bookingtype}
+                        >
+                            <FormControlLabel value="all" control={<Radio />} label="Show all" />
+                            <FormControlLabel value="timeinterval" control={<Radio />} label="Only Time Interval Bookings" />
+                            <FormControlLabel value="event" control={<Radio />} label="Only Event Bookings" />
+                        </RadioGroup>
+                    </FormControl>
+                </div>
+
+                <Box
+                    component="form"
+                    sx={{
+                        '& > :not(style)': { m: 1, width: '25ch' },
+                    }}
+                    noValidate
+                    autoComplete="off"                
+                >
+                    <TextField
+                        id="startfilter"
+                        label="Start Date"
+                        variant="standard"
+                        format={'YYYY/MM/DD'}
+                        type = "date"
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                    />
+                    <TextField
+                        id="endfilter"
+                        label="End Date"
+                        variant="standard"
+                        format={'YYYY/MM/DD'}
+                        type = "date"
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                    />
+                    <FormControl sx={{ minWidth: 258}}>
+                        <InputLabel>Type</InputLabel>
+                        <Select
+                        name='typefilter'
+                        value={this.state.typefilter}
+                        onChange={this.handleChange}
+                        variant="standard"
+                        >
+                            <MenuItem value={"Work"}>Work</MenuItem>
+                            <MenuItem value={"Flex Day"}>Flex Day</MenuItem>
+                            <MenuItem value={"Sick Day"}>Sick Day</MenuItem>
+                            <MenuItem value={"Vacation"}>Vacation</MenuItem>
+                            <MenuItem value={"Break"}>Break</MenuItem>
+                            <MenuItem value={"Project Work"}>Project Work</MenuItem>
+                        </Select>
+                    </FormControl>
+                    <Button
+                        onClick={this.setFilter}
                     >
-                        <MenuItem value={"Work"}>Work</MenuItem>
-                        <MenuItem value={"Flex Day"}>Flex Day</MenuItem>
-                        <MenuItem value={"Sick Day"}>Sick Day</MenuItem>
-                        <MenuItem value={"Vacation"}>Vacation</MenuItem>
-                        <MenuItem value={"Break"}>Break</MenuItem>
-                        <MenuItem value={"Project Work"}>Project Work</MenuItem>
-                    </Select>
-                </FormControl>
-                <Button>
-                    Filter
-                </Button>
+                        Filter Results
+                    </Button>
+                    <Button
+                        disabled = {this.state.showResetButton}
+                        onClick={this.resetFilter}
+                    >
+                        Remove Filter
+                    </Button>
+                    <Button
+                        onClick={this.printState}
+                    >
+                        Print State
+                    </Button>
+                </Box>
 
                 <Box sx={{width: '100%'}}>
                     <Paper sx={{width: '100%', mb: 2}}>
@@ -182,12 +385,20 @@ class MyBookings extends Component {
                                 <TableBody>
                                     {
                                         bookings.map( row =>
-                                        <TableRow>
+                                        <TableRow
+                                            hover true
+                                        >
+                                            {/* <TableCell>{row.type}</TableCell>
+                                            <TableCell>{row.start}</TableCell>
+                                            <TableCell>{row.end}</TableCell> */}
+
                                             <TableCell>{row.bookingtype}</TableCell>
                                             <TableCell>{row.type}</TableCell>
+                                            <TableCell>{row.date}</TableCell>
                                             <TableCell>{row.start}</TableCell>
                                             <TableCell>{row.end}</TableCell>
                                             <TableCell>{row.remarks}</TableCell>
+
                                         </TableRow>
                                         )
                                     }
