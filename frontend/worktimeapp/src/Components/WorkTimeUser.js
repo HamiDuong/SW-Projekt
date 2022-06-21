950b4cdbf00a48a0b70acdbf226876d24da264e8
vinamle <vl036@hdm-stuttgart.de>
2022-06-21 23:58:26 +0200

    Erstellung von WorkTimeUser Komponente

---
+++ frontend/worktimeapp/src/Components/WorkTimeUser.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+class WorkTimeUser extends Component {
+    constructor(props) {
+        super(props);
+    }
+    state = {  }
+    render() { 
+        return (
+            <>
+            </>
+        );
+    }
+}
+ 
+export default WorkTimeUser;
